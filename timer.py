296a7f59eaeab32a2ab1ebe6e07c73390e4cff23
h3racy <54757269+h3racy@users.noreply.github.com>
2019-09-01 14:23:02 +0900

    Update timer.py

--- timer.py
+++ timer.py
@@ -6,10 +6,10 @@ def timerx(p):
     global time
     time += 1
     print(time)
-    timer=threading.Timer(1, start_t, args=[p])
+    timer=threading.Timer(1, timerx, args=[p])
     timer.start()
     
     if time==p :
         timer.cancel()
         
-start_t(5)
+timerx(5)
