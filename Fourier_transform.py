e2a6fe9a6a88d39b4020d739fcac17d724f83e61
h3racy <54757269+h3racy@users.noreply.github.com>
2019-09-01 17:04:34 +0900

    Update Fourier_transform.py

--- Fourier_transform.py
+++ Fourier_transform.py
@@ -5,24 +5,23 @@ from matplotlib import pyplot as plt
 from math import sin, cos, pi, e
 import numpy as np
 
-l_time = 10
-bps = 8 #cycle
-time = 0
+l_time = 10000
+bps = 132 #cycle
 q = 0
 x = []
 y = []
 
-rcParams['figure.figsize']=(6, 6)
+rcParams['figure.figsize']=(12, 6)
 
-cycle = [0.01*i for i in range(0,100*l_time+1)]
+cycle = [0.0001*i for i in range(0,100*l_time+1)]
 
 #gt = [cos(b*pi*bps*2) for b in cycle] #g(t)
 #fx = [e**(2*1j*pi*t) for t in time] #f(x)
 
 while(q < 0.1) :
-    q += 0.001
-    time = [q*i for i in range(0, 10000)]
-    fg = [e**(2*1j*pi*t) * (1 + cos(b*pi*bps*2)) for t, b in zip(time, cycle)]
+    q += 0.0001
+    time = [q*i for i in range(0, 100000)]
+    fg = [e**(2*1j*pi*t) * (1 + cos(b*pi*bps*2)) for t, b in zip(time, cycle)] #f(x)g(x)
     fg = np.array(fg)
     p1 = sum(fg.real) / len(fg)
     p2 = sum(fg.imag) / len(fg)
