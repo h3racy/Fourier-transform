34348b29640caac675b3fc9688307acd39d897c2
h3racy <54757269+h3racy@users.noreply.github.com>
2019-09-01 13:44:45 +0900

    Update Fourier_transform.py

--- Fourier_transform.py
+++ Fourier_transform.py
@@ -1,21 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from matplotlib import pyplot, rcParams
-import math
-from math import sin, cos, pi, e
-from cmath import sqrt
+from math import cos, pi, e
+import numpy as np
 
-l_time = 2
-bps = 2
+l_time = 1
+bps = 3
 
 rcParams['figure.figsize']=(6, 3)
 
 x = [0.01*i for i in range(0,100*l_time+1)]
-y_cos = [cos(j*pi*bps*2)+1 for j in x] #g(t)
+a = [0.01*q for q in range(0, 100)]
 
+fx = [e**(2*1j*pi*t) for t in a] #f(x)
+gt = [cos(y*pi*bps*2)+1 for y in x] #g(t)
 
-bf = pow(e, -2*pi*1j)
+fx = np.array(fx)
+gt = np.array(gt)
 
+fxgt = [cos(y*pi*bps*2)*e**(2*1j*pi*t) for y, t in zip(x, a)]
 
-pyplot.plot(x, y_cos)
+fxgt = np.array(fxgt)
+
+pyplot.plot(x, gt)
 pyplot.show()
+pyplot.plot(fx.real, fx.imag)
+pyplot.show()
+pyplot.plot(fxgt.real, fxgt.imag)
