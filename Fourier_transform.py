84207f66077fdc9a5d9ba84eadd538f07a26df6f
h3racy <54757269+h3racy@users.noreply.github.com>
2019-09-01 16:56:17 +0900

    Update Fourier_transform.py

--- Fourier_transform.py
+++ Fourier_transform.py
@@ -5,24 +5,35 @@ from matplotlib import pyplot as plt
 from math import sin, cos, pi, e
 import numpy as np
 
-l_time = 4
-bps = 3 #cycle
+l_time = 10
+bps = 8 #cycle
 time = 0
+q = 0
+x = []
+y = []
 
 rcParams['figure.figsize']=(6, 6)
 
 cycle = [0.01*i for i in range(0,100*l_time+1)]
-time = [0.005*i for i in range(0, 1001)]
 
-gt = [cos(b*pi*bps*2) for b in cycle] #g(t)
-fx = [e**(2*1j*pi*t) for t in time] #f(x)
-fg = [e**(2*1j*pi*t) * (1 + cos(b*pi*bps*2)) for t, b in zip(time, cycle)]
+#gt = [cos(b*pi*bps*2) for b in cycle] #g(t)
+#fx = [e**(2*1j*pi*t) for t in time] #f(x)
 
-fx = np.array(fx)
-gt = np.array(gt)
-fg = np.array(fg)
+while(q < 0.1) :
+    q += 0.001
+    time = [q*i for i in range(0, 10000)]
+    fg = [e**(2*1j*pi*t) * (1 + cos(b*pi*bps*2)) for t, b in zip(time, cycle)]
+    fg = np.array(fg)
+    p1 = sum(fg.real) / len(fg)
+    p2 = sum(fg.imag) / len(fg)
+    x.append(p1)
+    y.append(p2)
 
-plt.plot(cycle, gt)
-plt.show()
-plt.plot(fx.real, fx.imag)
-plt.plot(fg.real, fg.imag)
+#fx = np.array(fx)
+#gt = np.array(gt)
+
+#plt.plot(cycle, gt)
+#plt.show()
+#plt.plot(fx.real, fx.imag)
+#plt.plot(fg.real, fg.imag)
+plt.plot(x, )
